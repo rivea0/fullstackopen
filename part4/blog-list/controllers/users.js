3f5cbfbd248c809f2d8d9498eef4040fe3746aef
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-03-23 20:06:31 +0300

    Expand blogs, complete 4.17

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
@@ -35,7 +35,7 @@ usersRouter.post('/', async (request, response, next) => {
   }
 
   const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
   const user = new User({
     username: body.username,
@@ -45,6 +45,7 @@ usersRouter.post('/', async (request, response, next) => {
 
   try {
     const savedUser = await user.save()
+
     response.status(201).json(savedUser)
   } catch(exception) {
     next(exception)
