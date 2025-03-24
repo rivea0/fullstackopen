621033033bebf5591d7e7c4c63da1d1d466fc33f
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-03-24 21:14:16 +0300

    Remove unnecessary import

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,7 +1,6 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 const User = require('../models/user')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
