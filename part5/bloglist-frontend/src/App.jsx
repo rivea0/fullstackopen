ae790bea460b76b8dd1e8b0d8f29690cfe1cb153
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-03-30 02:03:30 +0300

    Complete 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -166,7 +166,7 @@ const App = () => {
               </div>
             </>
           )}
-          {blogs.map(blog =>
+          {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
             <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
           )}
         </div>
