846dbe40a608a44a6f9779e7cd28057f2ba2b67d
rivea0 <58330360+rivea0@users.noreply.github.com>
2025-03-29 21:19:37 +0300

    Complete 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button type="button" onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+        {showDetails &&
+          <>
+            <div>{blog.url}</div>
+            <div>likes {blog.likes} <button type="button">like</button></div>
+            <div>{blog.user.name || blog.user.username}</div>
+          </>
+        }
+      </div>
+    </div>
+  )
+}
 
 export default Blog
